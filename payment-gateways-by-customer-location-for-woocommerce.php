5b9d2cc718bcd9559e2b97af3625b7b83b9c4f00
kousikmukherjee <kousikmukherjee.li@gmail.com>
2023-09-26 10:20:04 +0530

    v1.5.4

--- payment-gateways-by-customer-location-for-woocommerce.php
+++ payment-gateways-by-customer-location-for-woocommerce.php
@@ -3,12 +3,12 @@
 Plugin Name: Payment Gateways by Customer Location for WooCommerce
 Plugin URI: https://wpfactory.com/item/payment-gateways-by-customer-location-for-woocommerce/
 Description: Set countries, states, cities or postcodes to include/exclude for WooCommerce payment gateways to show up.
-Version: 1.5.3
+Version: 1.5.4
 Author: WPFactory
 Author URI: https://wpfactory.com
 Text Domain: payment-gateways-by-customer-location-for-woocommerce
 Domain Path: /langs
-WC tested up to: 7.8
+WC tested up to: 8.1
 */
 
 defined( 'ABSPATH' ) || exit;
@@ -29,7 +29,7 @@ if ( 'payment-gateways-by-customer-location-for-woocommerce.php' === basename( _
 	}
 }
 
-defined( 'ALG_WC_PGBCL_VERSION' ) || define( 'ALG_WC_PGBCL_VERSION', '1.5.2' );
+defined( 'ALG_WC_PGBCL_VERSION' ) || define( 'ALG_WC_PGBCL_VERSION', '1.5.4' );
 
 defined( 'ALG_WC_PGBCL_FILE' ) || define( 'ALG_WC_PGBCL_FILE', __FILE__ );
 
--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === Payment Gateways by Customer Location for WooCommerce ===
-Contributors: wpcodefactory, algoritmika, anbinder, omardabbas, kousikmukherjeeli
+Contributors: wpcodefactory, omardabbas, karzin, anbinder, algoritmika, kousikmukherjeeli
 Tags: woocommerce, payment gateway, location, woo commerce
 Requires at least: 4.4
-Tested up to: 6.2
-Stable tag: 1.5.3
+Tested up to: 6.3
+Stable tag: 1.5.4
 License: GNU General Public License v3.0
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -60,6 +60,11 @@ With [Pro version](https://wpfactory.com/item/payment-gateways-by-customer-locat
 1. Payment Gateways by Country.
 
 == Changelog ==
+
+= 1.5.4 - 26/09/2023 =
+* Tested up to: 6.3.
+* WC tested up to: 8.1.
+* Update plugin icon, banner.
 
 = 1.5.3 - 18/06/2023 =
 * WC tested up to: 7.8.
